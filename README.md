a3803cd31144cacd54840198f67b43e5915bcf80
anishLearnsToCode <anish_bt2k16@dtu.ac.in>
2020-09-01 15:07:11 +0530

    modifies README

--- README.md
+++ README.md
@@ -1,5 +1,32 @@
 # Mathematical Modelling & Simulation Lab
 
+
+
 | No. | Experiment | Date | Code |
 |-----|------------|------|------|
 | 1 | Find the solution of an Ordinary Differential Equation (ODE) | 21st August 2020 | [![matlab](https://img.icons8.com/nolan/40/matlab.png)](lab/experiment-01/experiment_1.m)|
+| 2 | Perform Linear Regression and fit a curve in a linear equation | 28th August 2020 | |
+
+
+## Running Projects Locally
+Clone this repository on your machine and enter the project directory.
+```bash
+git clone https://github.com/anishLearnsToCode/mathematical-modelling-lab.git
+cd mathematical-modelling-lab/lab/
+```
+
+Navigate to a project of your choice from the list below and run it as follows e.g. to run 
+experiment 1 
+```bash
+cd experiment-01
+matlab -nosplash -nodesktop -r "run('experiment_1.m');"
+``` 
+
+Similarly you can run any experiment in the lab by executing a similar expession as:
+
+```bash
+cd experiment-n
+matlab -nosplash -nodesktop -r "run('experiment_n.m');"
+```   
+
+where `n` stands for the experiment number.
---
+++ lab/experiment-02/linear_regression.m
@@ -0,0 +1,36 @@
+% Lab 2
+% Linear Regression
+
+clc; 
+clear; 
+close all;
+
+
+function J = computeCost(X, y, theta)
+%COMPUTECOST Compute cost for linear regression
+%   J = COMPUTECOST(X, y, theta) computes the cost of using theta as the
+%   parameter for linear regression to fit the data points in X and y
+
+  trainingExamles = size(X)(1);
+  predictions = X * theta;
+  squareErrors = (predictions - y) .^ 2;
+  J = 0.5 / trainingExamles * sum(squareErrors);
+end
+
+
+function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
+  %GRADIENTDESCENT Performs gradient descent to learn theta
+  %   theta = GRADIENTDESCENT(X, y, theta, alpha, num_iters) updates theta by 
+  %   taking num_iters gradient steps with learning rate alpha
+
+  % Initialize some useful values
+  trainingExamples = length(y); % number of training examples
+  J_history = zeros(num_iters, 1);
+  features = length(theta) - 1;
+
+  for iter = 1:num_iters
+     theta = theta - alpha * (1/trainingExamples) * (((X*theta) - y)' * X)'; % Vectorized  
+     J_history(iter) = computeCost(X, y, theta);
+  end
+end
+
